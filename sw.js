34d4d587948fe0782ed7c81911fc3b614a617ea5
leomaizieres <75469904+leomaizieres@users.noreply.github.com>
2022-01-08 16:53:32 +0100

    Reupload of my website after changes

---
+++ sw.js
@@ -0,0 +1,45 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js', { scope: './' }).then(function(reg) {
+		console.log('Registration succeeded. Scope is ' + reg.scope);
+		}).catch(function(error) {
+		console.log('Registration failed with ' + error);
+	});
+};
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('v1').then(function(cache) {
+			return cache.addAll([
+				'/animation/',
+				'/config/',
+				'/design/',
+				'/favicon/',
+				'/images/',
+				'/site anime/',
+				'/style/',
+				'/webdesign/',
+				'/CV.pdf',
+				'/favicon.ico',
+				'/index.html',
+				'/jquery-3.5.1.min.js',
+				'/manifest.json',
+				'/temp.htaccess',
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+		caches.match(e.request).then((r) => {
+			console.log('[Service Worker] Récupération de la ressource: '+e.request.url);
+			return r || fetch(e.request).then((response) => {
+                return caches.open('v1').then((cache) => {
+					console.log('[Service Worker] Mise en cache de la nouvelle ressource: '+e.request.url);
+					cache.put(e.request, response.clone());
+					return response;
+				});
+			});
+		})
+	);
+});
